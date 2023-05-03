ad1a403fc679a8939271619c99562c1da43d3304
unknown <michaelmolina679@gmail.com>
2023-05-03 11:26:30 -0600

    clase condicional ternario

---
+++ src/bases/12-ternario.js
@@ -0,0 +1,20 @@
+// Operador Condicional Ternario
+
+const activo = true;
+
+// Utilzando un if normal
+// let mensaje = '';
+// if(activo) {
+//     mensaje = "activo";
+// } else {
+//     mensaje = "inactivo";
+// }
+
+// Usando condicional ternario (if-else)
+// const mensaje = activo ? 'activo' : 'inactivo';
+
+// Condicional ternario con una sola condicion (solo if)
+const mensaje = activo && 'activo';
+
+
+console.log(mensaje);
